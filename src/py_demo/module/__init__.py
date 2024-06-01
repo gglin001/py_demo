75c368833f2274eead1c5e262361b29c3a7b62c4
Allen Guo <guosonglin001@gmail.com>
2024-06-01 23:03:31 +0800

    wip

--- src/py_demo/module/__init__.py
+++ src/py_demo/module/__init__.py
@@ -1,1 +1,3 @@
+from __future__ import annotations
+
 from .function import exp_func  # noqa: F401
--- tests/test_method.py
+++ tests/test_method.py
@@ -1,5 +1,6 @@
-import numpy as np
+from __future__ import annotations
 
+import numpy as np
 import py_demo
 
 
