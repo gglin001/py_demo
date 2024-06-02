4ec4b0a7b75c8b982b712de2a254d2b9e8604e9b
Allen Guo <guosonglin001@gmail.com>
2024-06-02 15:02:10 +0800

    wip

--- scripts/cibuildwheel.sh
+++ scripts/cibuildwheel.sh
@@ -4,6 +4,14 @@
 cibuildwheel . --archs all --platform linux --print-build-identifiers
 cibuildwheel . --archs all --platform macos --print-build-identifiers
 
+# enable targets:
+# cp311-manylinux_x86_64
+# cp312-manylinux_x86_64
+# cp311-manylinux_aarch64
+# cp312-manylinux_aarch64
+# cp311-macosx_arm64
+# cp312-macosx_arm64
+
 # macos with colima(docker) or linux
 cibuildwheel . --archs aarch64 --platform linux
 cibuildwheel . --archs x86_64 --platform linux
